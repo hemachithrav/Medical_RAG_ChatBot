8fb72e867c834a16d5e5367cc8217d0da2b8da03
hemachithrav <hemachithra31011999@gmail.com>
2026-04-09 00:22:40 +0530

    Updated requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical_RAG_ChatBot",
+    version="0.1.0",
+    author="Hemachithra",
+    packages=find_packages(),
+    install_requires=[]
+
+)
