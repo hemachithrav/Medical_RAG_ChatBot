7eeeea232ad7dda3864373852b6dc1a27ba9a40d
hemachithrav <hemachithra31011999@gmail.com>
2026-04-16 23:56:17 +0530

    changes updated library

--- src/helper.py
+++ src/helper.py
@@ -2,7 +2,7 @@ from langchain_community.document_loaders import PyPDFLoader,DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from typing import List
-from langchain.schema import Document
+from langchain_core.documents import Document
 
 
 #Extract Data From the PDF File.
